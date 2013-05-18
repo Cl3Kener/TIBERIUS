41a64eb464611ad11b206e22511376a548edffc4
Cl3Kener <you_didnt_see_me_here@hotmail.com>
2013-05-18 16:45:55 -0600

    Fixes
    Add/Fixup CPU Govs
    
    AssWax

--- drivers/cpufreq/cpufreq_asswax.c
+++ drivers/cpufreq/cpufreq_asswax.c
@@ -73,7 +73,7 @@ static unsigned int sleep_ideal_freq;
  * Zero disables and causes to always jump straight to max frequency.
  * When below the ideal freqeuncy we always ramp up to the ideal freq.
  */
-#define DEFAULT_RAMP_UP_STEP 460800
+#define DEFAULT_RAMP_UP_STEP 468000
 static unsigned int ramp_up_step;
 
 /*
