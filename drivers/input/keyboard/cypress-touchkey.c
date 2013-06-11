bdc9a38c599bbf13d792a2dfc57f5f78ca542694
Cl3Kener <cl3kener@gmail.com>
2013-06-10 18:19:11 -0600

    Revert "fixed the BLN kernel config option"
    This reverts commit 0df52180f445b77abb0173ff542c2c6171f72742.
    
    Conflicts:
    	drivers/input/keyboard/Kconfig

--- drivers/input/keyboard/cypress-touchkey.c
+++ drivers/input/keyboard/cypress-touchkey.c
@@ -71,8 +71,9 @@ Cypress touchkey register
 #define BUIL_FW_VER	0x05
 #endif
 
-//#define CONFIG_KEYPAD_CYPRESS_TOUCH_BLN
-#if defined(CONFIG_KEYPAD_CYPRESS_TOUCH_BLN)
+#define CONFIG_TOUCHKEY_BLN
+#ifdef CONFIG_TOUCHKEY_BLN
+#include <linux/miscdevice.h>
 #include <linux/wakelock.h>
 #define BLN_VERSION 9
 
@@ -472,7 +473,7 @@ void touchkey_resume_func(struct work_struct *p)
 
 static irqreturn_t touchkey_interrupt(int irq, void *dummy)  // ks 79 - threaded irq(becuase of pmic gpio int pin)-> when reg is read in work_func, data0 is always release. so temporarily move the work_func to threaded irq.
 {
-#if defined(CONFIG_KEYPAD_CYPRESS_TOUCH_BLN)
+#ifdef CONFIG_TOUCHKEY_BLN
         printk(KERN_ERR "[TouchKey] interrupt touchkey\n");
 #endif
     u8 data[3];
@@ -1017,7 +1018,7 @@ schedule_delayed_work(&touch_resume_work, msecs_to_jiffies(500));
 }
 #endif				// End of CONFIG_HAS_EARLYSUSPEND
 
-#if defined(CONFIG_KEYPAD_CYPRESS_TOUCH_BLN)
+#ifdef CONFIG_TOUCHKEY_BLN
 
 static void touchkey_activate(void){
 
@@ -1027,6 +1028,7 @@ static void touchkey_activate(void){
         }
 
         printk(KERN_DEBUG "[TouchKey] touchkey activate.\n");
+        //touchkey_ldo_on(1);
 		tkey_vdd_enable(1);
 		tkey_led_vdd_enable(1); 
 		
@@ -1035,6 +1037,8 @@ static void touchkey_activate(void){
 
 static void touchkey_deactivate(void){
 
+        //touchkey_led_ldo_on(0);
+        //touchkey_ldo_on(0);
         tkey_vdd_enable(0);
 		tkey_led_vdd_enable(0);		
 
@@ -1393,7 +1397,7 @@ if (get_hw_rev() >=0x02) {
 }
 #endif
 	set_touchkey_debug('K');
-#if defined(CONFIG_KEYPAD_CYPRESS_TOUCH_BLN)
+#ifdef CONFIG_TOUCHKEY_BLN
         err = misc_register( &bln_device );
         if( err ){
             printk(KERN_ERR "[BLN] sysfs misc_register failed.\n");
@@ -1624,7 +1628,7 @@ static ssize_t touch_led_control(struct device *dev, struct device_attribute *at
 	int int_data = 0;
 	int errnum = 0;
 
-#if defined(CONFIG_KEYPAD_CYPRESS_TOUCH_BLN)
+#ifdef CONFIG_TOUCHKEY_BLN
 	printk(KERN_ERR "[TouchKey] system calling LED Notification control\n");
 #endif
 
@@ -2878,7 +2882,7 @@ static void __exit touchkey_exit(void)
 	i2c_del_driver(&touchkey_i2c_driver);
 	misc_deregister(&touchkey_update_device);
 	
-#if defined(CONFIG_KEYPAD_CYPRESS_TOUCH_BLN)
+#ifdef CONFIG_TOUCHKEY_BLN
         misc_deregister(&bln_device);
         wake_lock_destroy(&bln_wake_lock);
 #endif
